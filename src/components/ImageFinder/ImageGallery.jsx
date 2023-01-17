53e1378aedac624902941453a2ca1f36de83e23c
Tiomasik <a.masliy@ukr.net>
2023-01-17 14:46:48 +0200

    corrected mistakes

--- src/components/ImageFinder/ImageGallery.jsx
+++ src/components/ImageFinder/ImageGallery.jsx
@@ -4,24 +4,23 @@ import ImageGalleryItem from './ImageGalleryItem'
 
 const ImageGallery = ({ listSearch, choseItem }) => (
     <ul className='ImageGallery'>
-        {listSearch.map(({ id, webformatURL, tags }) =>
-            <li key={id} onClick={() => choseItem(id)}  className='ImageGalleryItem'>
-                <ImageGalleryItem webformatURL={webformatURL}
-                    tags={ tags } />
+        {listSearch.map(list =>
+            <li key={list.id} onClick={() => choseItem(list.id)}  className='ImageGalleryItem'>
+                <ImageGalleryItem webformatURL={list.webformatURL}
+                    tags={ list.tags } />
             </li>)}
     </ul>
 )
 
 ImageGallery.propTypes = {
     listSearch: PropTypes.arrayOf(
-        PropTypes.object.isRequired,
-        PropTypes.exact({
-            id: PropTypes.number.isRequired,
-            webformatURL: PropTypes.string.isRequired,
-            tags: PropTypes.string.isRequired,
-        }),
-    ),
+            PropTypes.shape({
+                id: PropTypes.number.isRequired,
+                webformatURL: PropTypes.string.isRequired,
+                tags: PropTypes.string.isRequired,
+            })),
     choseItem: PropTypes.func.isRequired,
 }
+
 
 export default ImageGallery
